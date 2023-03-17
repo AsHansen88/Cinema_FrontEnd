d697f34ffdca52fa0dbc9176f33529fee038420f
CodeClod <mika9103@stud.kea.dk>
2023-03-17 14:47:31 +0100

    Changes by lenovo

--- js/fetchMovies.js
+++ js/fetchMovies.js
@@ -1,4 +1,4 @@
-console.log("Vi er i fetchMovies fra Database")
+/*console.log("Vi er i fetchMovies fra Database")
 const urlMovies = "http://localhost:8080/Movies"
 const urlPostMovies = "http://localhost:8080/cinema_database"
 
@@ -50,4 +50,42 @@ const pbPostMovies = document.getElementById("pbPostMovies")
 pbFetchMovies.addEventListener('click', actionFetchMovies)
 pbPostMovies.addEventListener('Click', actionPostAllMovies)
 
+*/
+import { imageBaseURL } from "./api.js";
 
+
+export function createMovieCard(movie) {
+
+    const {
+        poster_path,
+        title,
+        vote_average,
+        release_date,
+        id
+    } = movie;
+
+    const card = document.createElement("div");
+    card.classList.add("movie-card");
+
+    card.innerHTML = `
+    <figure class="poster-box card-banner">
+      <img src="${imageBaseURL}w342${poster_path}" alt="${title}" class="img-cover" loading="lazy">
+    </figure>
+    
+    <h4 class="title">${title}</h4>
+    
+    <div class="meta-list">
+      <div class="meta-item">
+        <img src="./assets/images/star.png" width="20" height="20" loading="lazy" alt="rating">
+    
+        <span class="span">${vote_average.toFixed(1)}</span>
+      </div>
+    
+      <div class="card-badge">${release_date.split("-")[0]}</div>
+    </div>
+    
+    <a href="./detail.html" class="card-btn" title="${title}" onclick="getMovieDetail(${id})"></a>
+  `;
+
+    return card;
+}
